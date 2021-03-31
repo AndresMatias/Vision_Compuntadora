0d6ee4a06341f1d3ee147139e119070f72797f0f
andres <andres.singls@gmail.com>
2021-03-30 21:06:59 -0300

    1)Practico 1

---
+++ Practico_1/Practico_1.py
@@ -0,0 +1,24 @@
+import random
+def adivinar(intentos): #Funcion adivinar
+	intentos_max=3
+	if intentos<=intentos_max:
+		nro1=int(input('Ingrese un numero entero para adivinar: '))
+		#nro2=random.randint(0, 100); #Numero a adivinar
+		nro2=3
+		print(str(nro1))
+		if nro1==nro2 :
+			print("Adivinaste en el intento "+str(intentos))
+			return(10)
+		else:
+			print("Fallaste, tenes "+str(intentos_max-intentos) +" intentos")
+			return(0)
+	else:
+		print("Usaste todos los intentos")	
+		return(10)
+#-----------Fin de Funcion-----------------
+
+for i in range(1,4):
+	if adivinar(i) == 10:
+		break
+else:
+	print("Fin del Programa")
